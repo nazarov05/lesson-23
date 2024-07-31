8d562d457b340aaaf3dc7bfb747a308a5683f635
Hojiakbar <hojiakbarn976@gmail.com>
2024-07-31 11:41:57 +0300

    edbrdf

---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+// import { React } from "react";
+// import "./Navbar.css"
+
+// function Navbar() {
+
+   
+//     return(
+//         <div className="navbar">
+//             <div className="navbar-bottom">
+//             <div className="logo">logo</div>
+//             <input className="input" placeholder="Search..." type="text" />
+//             <div className="menu">Menu</div>
+//             </div>
+//         <div className="navbar-header">
+//                 <div className="navbar-box">
+//                   <input className="input2" type="text" placeholder="Search..." />
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+// export default Navbar;
+
+
+
+
+
+import { React } from "react";
+import "./Navbar.css"
+
+function Navbar() {
+
+    return(
+
+    )
+}
+
+export default Navbar;
